c5f6cfb8f1ffdcda3a2e0e04d920490d9f2cbd0c
Erik Alvarez Tyldum <erik@garnesdata.no>
2019-04-26 00:19:36 +0200

    Fixed release

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #define SCREEN_HEIGHT 600
 #define SCREEN_WIDTH 1100
 
+#if(DEBUG) 
 int main(int argc, char* argv[])
 {
 	std::unique_ptr<Game> game(new Game());
@@ -14,10 +15,10 @@ int main(int argc, char* argv[])
 	const std::string fontPath = ".\\fonts\\arcade.ttf";
 
 	//initialize game --> Set last flag to true if you want fullscreen
-	if (game->init_window( "Arkanoid",
+	if (game->init_window("Arkanoid",
 		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
 		SCREEN_WIDTH, SCREEN_HEIGHT, false)
-		&& game->init_font(fontPath) 
+		&& game->init_font(fontPath)
 		&& game->init_music())
 	{
 		game->run();
@@ -25,3 +26,24 @@ int main(int argc, char* argv[])
 
 	return 0;
 }
+#else
+int WinMain()
+{
+	std::unique_ptr<Game> game(new Game());
+
+	const std::string fontPath = ".\\fonts\\arcade.ttf";
+
+	//initialize game --> Set last flag to true if you want fullscreen
+	if (game->init_window("Arkanoid",
+		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		SCREEN_WIDTH, SCREEN_HEIGHT, false)
+		&& game->init_font(fontPath)
+		&& game->init_music())
+	{
+		game->run();
+	}
+
+	return 0;
+}
+#endif
+
